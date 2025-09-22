b80cb1ad108c3779a920c6397cf2f6c550772acf
Julio Celestino <velocirraptorrex3@gmail.com>
2025-09-21 23:01:20 -0500

    Julio Celestino
    add parent scripts.

---
+++ Building.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio1Examen1
+{
+    internal class Building
+    {
+        internal class Structure
+        {
+            protected string name { get; }
+            protected int price { get; }
+            protected int health { get; set; }
+
+            protected Structure(string name, int price, int health)
+            {
+                this.name = name;
+                this.price = price;
+                this.health = health;
+            }
+            protected virtual void TakeDamage(int damage)
+            {
+                health -= damage;
+                if (health < 0) health = 0;
+            }
+            protected virtual string GetInfo()
+            {
+                return $"{name} Cuesta: {price} Salud: {health}";
+            }
+        }
+    }
+
+}
---
+++ Unit.cs
@@ -0,0 +1,35 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Promedio1Examen1
+{
+    internal class Unit
+    {
+        protected string name { get; }
+        protected int dange { get; }
+        protected int health { get; set; }
+        protected int speed { get; }
+        protected int price { get;  }
+
+        protected Unit(string name, int dange, int health, int speed, int price)
+        {
+            this.name = name;
+            this.dange = dange;
+            this.health = health;
+            this.speed = speed;
+            this.price = price;
+        }
+        protected virtual void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health < 0) health = 0;
+        }
+        protected virtual string GetInfo()
+        {
+            return $"{name} Cuesta: {price} Salud: {health} Daño: {dange} Velocidad: {speed}";
+        }
+    }
+}
