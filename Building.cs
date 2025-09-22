4314c6229a8b6a57156c00b0c69ebef48ee49590
Julio Celestino <velocirraptorrex3@gmail.com>
2025-09-22 00:44:05 -0500

    Julio Celestino
    Corrections

--- Building.cs
+++ Building.cs
@@ -8,28 +8,24 @@ namespace Promedio1Examen1
 {
     internal class Building
     {
-        internal class Structure
-        {
-            protected string name { get; }
-            protected int price { get; }
-            protected int health { get; set; }
+        protected string name { get; }
+        protected int price { get; }
+        protected int health { get; set; }
 
-            protected Structure(string name, int price, int health)
-            {
-                this.name = name;
-                this.price = price;
-                this.health = health;
-            }
-            protected virtual void TakeDamage(int damage)
-            {
-                health -= damage;
-                if (health < 0) health = 0;
-            }
-            protected virtual string GetInfo()
-            {
-                return $"{name} Cuesta: {price} Salud: {health}";
-            }
+        protected Building(string name, int price, int health)
+        {
+            this.name = name;
+            this.price = price;
+            this.health = health;
+        }
+        protected virtual void TakeDamage(int damage)
+        {
+            health -= damage;
+            if (health < 0) health = 0;
+        }
+        protected virtual string GetInfo()
+        {
+            return $"{name} Cuesta: {price} Salud: {health}";
         }
     }
-
 }
